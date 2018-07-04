d6020e4ebddf2420d8599e3b293bb4c06a88293b
fabio guelfi <fabioguelfiunix@gmail.com>
2018-07-03 21:51:20 -0300

    1-exemplo

---
+++ 1-exemplo/olaMundo.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * 1 - Executar: shift + cmd + b
+ * 2 - No terminal executar node + nome do arquivo sem a extensao
+ */
+var OlaMundo = /** @class */ (function () {
+    function OlaMundo() {
+    }
+    OlaMundo.prototype.digaOlaTodo = function () {
+        console.log("Fala coders");
+    };
+    return OlaMundo;
+}());
+exports.OlaMundo = OlaMundo;
+var olaMundo = new OlaMundo();
+olaMundo.digaOlaTodo();
+//# sourceMappingURL=olaMundo.js.map
---
+++ 1-exemplo/olaMundo.ts
@@ -0,0 +1,14 @@
+/**
+ * 1 - Executar: shift + cmd + b
+ * 2 - No terminal executar node + nome do arquivo sem a extensao
+ */
+export class OlaMundo {
+
+    public digaOlaTodo(): void {
+        console.log(`Fala coders`);
+    }
+
+}
+
+let olaMundo = new OlaMundo();
+olaMundo.digaOlaTodo();
